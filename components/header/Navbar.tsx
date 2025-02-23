2386ef775e051164905d7d348d7c998fcdfb7eab
Brook  Worku Alemu <109297876+BrookW-83@users.noreply.github.com>
2025-02-23 18:23:57 +0300

    chore: navbar and profile components
    - Add icons and update
    
    - Implement header profile section

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { GoHome } from "react-icons/go";
+import { MdOutlinePeopleAlt } from "react-icons/md";
+import { CiCalendar } from "react-icons/ci";
+import { FiMessageSquare } from "react-icons/fi";
+import { IoMdCard } from "react-icons/io";
+
+
+const NavbarOption = [
+  {
+    id: 1,
+    name: "Overview",
+    icon: <GoHome/>,
+  },
+  {
+    id: 2,
+    name: "Patient",
+    icon: <MdOutlinePeopleAlt/>,
+  },
+  {
+    id: 3,
+    name: "Schedule",
+    icon: <CiCalendar/>,
+  },
+  {
+    id: 2,
+    name: "Message",
+    icon: <FiMessageSquare/>,
+  },
+  {
+    id: 3,
+    name: "Transaction",
+    icon: <IoMdCard/>,
+  },
+];
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const [activeRoute, setActiveRoute] = useState("Patient");
+
+  return (
+    <nav className="flex space-x-2">
+      {NavbarOption.map((route) => (
+        <div
+          key={route.name}
+          onClick={() => setActiveRoute(route.name)}
+          className={`flex items-center gap-2 p-2 cursor-pointer ${
+            activeRoute === route.name
+              ? "bg-green-500 text-grey-700 rounded-full"
+              : "text-gray-700"
+          }`}
+        >
+          {route.icon}
+          <span className="text-sm font-medium">{route.name}</span>
+        </div>
+      ))}
+    </nav>
+  );
 };
 
 export default Navbar;
--- components/header/Profile.tsx
+++ components/header/Profile.tsx
@@ -1,5 +1,22 @@
+import { IoSettingsOutline } from "react-icons/io5";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex items-center gap-4 p-2">
+        <img src="/header/profile.png" alt="Profile" className="w-10 h-10 rounded-full" />
+        <div className="flex flex-col">
+          <span className="text-sm font-medium">Dr. Jose Simmons</span>
+          <span className="text-xs text-gray-500">General Practitioner</span>
+        </div>
+        <div className="flex items-center gap-2 ml-auto">
+          <div className="w-px h-6 bg-gray-300"></div>
+          <IoSettingsOutline className="w-5 h-5 text-gray-700 cursor-pointer" />
+          <BsThreeDotsVertical className="w-5 h-5 text-gray-700 cursor-pointer" />
+          
+        </div>
+      </div>
+  )
 };
 
 export default Profile;
